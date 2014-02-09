06da50b5a89e73ddb57c371c58783cd18eed473c
Tom <Tom@Airi-Petteri.local>
2014-02-09 14:27:42 +0200

    Fusion of files, structuring the database, creating new database functions, etc.. Just lots of stuff.

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,129 @@
+// CREATE AND INITIALIZE DATABASE
+
+var db = new ydn.db.Storage('personal-travis-db');
+
+function writeRecord(exerciseName, weight, reps, exclude)
+{
+  var d = new Date();
+  var currDay = ('0'+(d.getDate())).slice(-2);
+  var currMonth = ('0'+(d.getMonth()+1)).slice(-2);
+  var currYear = d.getFullYear();
+  var now = currYear +"-"+ currMonth +"-"+ currDay;
+  alert(exerciseName + weight + reps + exclude);
+	db.put(exerciseName, {weight: weight, reps: reps, exclude: exclude}, now);
+}
+
+function writeDefaultData()
+{
+	/* benchpress = pena, inclinepress = vinopena, lyingtricepsextension = ranskis */
+	writeRecord("benchpress", 30, 10, true);
+	writeRecord("inclinepress", 20, 10, true);
+	writeRecord("lyingtricepsextension", 15, 10, true);
+	
+	/* cablepulldown = ylätalja, cableseatedrow = alatalja, bicepscurl = hauiskääntö */
+	writeRecord("cablepulldown", 20, 10, true);
+	writeRecord("cableseatedrow", 20, 10, true);
+	writeRecord("bicepscurl", 4, 10, true);
+	
+	/* deadlift = maastoveto, legextension = jalanojennus, legcurl = jalankoukistus */
+	writeRecord("deadlift", 30, 10, true);
+	writeRecord("legextension", 10, 10, true);
+	writeRecord("legcurl", 10, 10, true);
+	
+	/* uprightrow = pystysoutu, shoulderpress = pystypunnerrus, shrugs = olankohautus */
+	writeRecord("uprightrow", 20, 10, true);
+	writeRecord("shoulderpress", 20, 10, true);
+	writeRecord("shrugs", 10, 10, true);
+	
+	/* bentoverrow = kulmasoutu, pecdeck = rintarutistus, hammerbicepscurl = vasarakääntö */
+	writeRecord("bentoverrow", 25, 10, true);
+	writeRecord("pecdeck", 20, 10, true);
+	writeRecord("hammerbicepscurl", 4, 10, true);
+	
+	/* legpress = jalkaprässi, lunge = askelkyykky, powerclean = rive */
+	writeRecord("legpress", 50, 10, true);
+	writeRecord("lunge", 20, 10, true);
+	writeRecord("powerclean", 25, 10, true);
+}
+
+function getReps(exercise)
+{
+	db.from(exercise).list().done(function(records) 
+	{
+		console.log(records[(records.length - 1)].reps);
+	});
+}
+
+function getWeight(exercise)
+{
+	db.from(exercise).list().done(function(records) 
+	{
+		console.log(records[(records.length - 1)].weight);
+	});
+}
+
+function checkLaunchCounter()
+{
+	launchCounter = parseInt(localStorage.getItem("launchCounter"));
+	if (launchCounter === null) 
+	{
+		localStorage.setItem("launchCounter", 1);
+		localStorage.setItem("lastWorkout", 6);
+		console.log("The first launch");
+	}
+	else
+	{
+		localStorage.setItem("launchCounter", launchCounter + 1);
+		console.log("The launch number " + launchCounter);
+	}
+}
+
+// suggestNextWorkout()
+// {
+//   var lastWorkout = parseInt(localStorage.setItem("lastWorkout"));
+//   if (lastWorkout == 6)
+//   {
+//     // lisää suggested tiiliin 1
+//   }
+//   else
+//   {
+//     var nextWorkout = lastWorkout + 1;
+//     // lisää suggested tiiliin nextWorkout
+//   }
+// }
+
+/* INTERVALS */
+
+var interval = 
+{
+  benchpress: 5, // penkkipunnerrus
+  inclinepress: 10, // vinopenkkipunnerrus
+  lyingtricepsextension: 2.5, // ranskalainen punnerrus
+  cablepulldown: 5, // ylätalja
+  cableseatedrow: 5, // alatalja
+  bicepscurl: 1, // hauiskääntö
+  deadlift: 5, // maastaveto
+  legextension: 5, // jalanojennus
+  legcurl: 5, // jalankoukistus
+  uprightrow: 5, // pystysoutu
+  shoulderpress: 5, // pystypunnerrus
+  shrugs: 2.5, // olankohautus
+  bentoverrow: 2.5, // kulmasoutu
+  pecdeck: 5, // rintarutistus
+  hammerbicepscurl: 1, // vasarakääntö
+  legpress: 10, // jalkaprässi
+  lunge: 10, // askelkyykky
+  powerclean: 5 // rinnalleveto
+}
+
 /* DECLARE DATA FETCHING FUNCTIONS */
   
 function exerciseObject(exerciseName)
 {
 	this.name = exerciseName;
-	this.reps = 12;
-	this.weight = 10;
+	this.reps = 0;
+	this.weight = 0;
 	this.reached = false;
-	this.interval = 5;
 	
 	this.updateReps = updateReps;
 	function updateReps(reps)
@@ -53,7 +170,7 @@ var powerclean            = new exerciseObject("powerclean");
 function initFirstLevelView()
 {
   $("#second-level-view").fadeOut();
-  suggestNextWorkout();
+  //suggestNextWorkout();
 }
 
 function printExercise(exerciseName)
@@ -144,6 +261,7 @@ function printWorkout(workoutNo)
 	  "  <img class='navi-icon suggested' id='remember' src='img/navi-icons/remember.png' onclick='rememberWorkout(" + workoutNo + ")' />" +
 	  "  <img class='navi-icon' id='forget' src='img/navi-icons/forget.png' onclick='initFirstLevelView()' />" +
 	  "  <img class='navi-icon' id='edit' src='img/navi-icons/edit.png' onclick='editWorkoutGoals()' />" +
+	  "  <span class='sub-title'>goals</span>" +
 	  "</div>"
 	);
 	switch(workoutNo)
---
+++ js/strings.js
@@ -0,0 +1,121 @@
+// Language detection code is based on Michael Herger's solution. Check
+// details: https://groups.google.com/forum/#!topic/phonegap/EkCyEBO8Dj4
+// PhoneGap on Android would always return EN in navigator.*language, so
+// let's parse userAgent instead.
+
+var lang;
+
+if (navigator && navigator.userAgent && (lang = navigator.userAgent.match(/android.*\W(\w\w)-(\w\w)\W/i))) 
+{
+	lang = lang[1];
+}
+
+if (!lang && navigator) 
+{
+	if (navigator.language) 
+	{
+		lang = navigator.language;
+	} 
+	else if (navigator.browserLanguage) 
+	{
+		lang = navigator.browserLanguage;
+	} 
+	else if (navigator.systemLanguage) 
+	{
+		lang = navigator.systemLanguage;
+	} 
+	else if (navigator.userLanguage) 
+	{
+		lang = navigator.userLanguage;
+	}
+	lang = lang.substr(0, 2);
+}
+
+var string;
+
+if (lang == "fi") /* FINNISH */
+{
+  string =
+  {
+		/* training 1 */
+		benchpress: "penkkipunnerrus",
+		inclinepress: "vinopenkkipunnerrus",
+		lyingtricepsextension: "ranskalainen punnerrus",
+
+		/* training 2 */
+		cablepulldown: "ylätalja",
+		cableseatedrow: "alatalja",
+		bicepscurl: "hauiskääntö",
+
+		/* training 3 */
+		deadlift: "maastaveto",
+		legextension: "jalanojennus",
+		legcurl: "jalankoukistus",
+
+		/* training 4 */
+		uprightrow: "pystysoutu",
+		shoulderpress: "pystypunnerrus",
+		shrugs: "olankohautus",
+
+		/* training 5 */
+		bentoverrow: "kulmasoutu",
+		pecdeck: "rintarutistus",
+		hammerbicepscurl: "vasarakääntö",
+
+		/* training 6 */
+		legpress: "jalkaprässi",
+		lunge: "askelkyykky",
+		powerclean: "rinnalleveto",
+
+		/* general UI-related strings */
+		personalTravis: "personal raineri",
+		nextWorkout: "seuraava treeni",
+		goals: "haasteet",
+		reps: "toistot",
+		weight: "paino",
+		achieved: "saavutettu"
+	}
+}
+else /* ENGLISH */
+{
+  string =
+  {
+		/* training 1 */
+		benchpress: "benchpress",
+		inclinepress: "incline press",
+		lyingtricepsextension: "lying triceps extension",
+
+		/* training 2 */
+		cablepulldown: "cable pulldown",
+		cableseatedrow: "cable seated row",
+		bicepscurl: "biceps curl",
+
+		/* training 3 */
+		deadlift: "deadlift",
+		legextension: "leg extension",
+		legcurl: "leg curl",
+
+		/* training 4 */
+		uprightrow: "upright row",
+		shoulderpress: "shoulder press",
+		shrugs: "shrugs",
+
+		/* training 5 */
+		bentoverrow: "bent-over row",
+		pecdeck: "pec deck",
+		hammerbicepscurl: "hammer biceps curl",
+
+		/* training 6 */
+		legpress: "leg press",
+		lunge: "lunge",
+		powerclean: "power clean",
+
+		/* general UI-related strings */
+		personalTravis: "personal travis",
+		nextWorkout: "next workout",
+		goals: "goals",
+		reps: "reps",
+		weight: "weight",
+		achieved: "achieved"
+	}
+}
