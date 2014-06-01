e6f0980911b8ca42a668c9346393aab80661feea
Tom <Tom@Airi-Petteri-2.local>
2014-06-01 23:45:04 +0300

    Let's see if this makes ga work

--- js/main.js
+++
@@ -1,9 +0,0 @@
-/* Google Analytics PhoneGap plugin creates the object window.plugins.gaPlugin */
-var gaPlugin;
-
-function onDeviceReady() 
-{
-  /* More info about usage of gaPlugin at github.com/phonegap-build/GAPlugin */
-  gaPlugin = window.plugins.gaPlugin;
-  gaPlugin.init(successHandler, errorHandler, "UA-51549024-1", 10);
-}
