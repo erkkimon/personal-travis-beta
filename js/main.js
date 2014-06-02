11d6f50baca4866d5ed35791bdff5879664b5297
Tom <Tom@Airi-Petteri-2.local>
2014-06-02 17:19:42 +0300

    Alerts deleted

--- js/main.js
+++ js/main.js
@@ -2,32 +2,32 @@ var gaPlugin;
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
-function onDeviceReady() {
-    alert("calling ga");
-    gaPlugin = window.plugins.gaPlugin;
-    alert("calling init")
-    gaPlugin.init(successHandler, errorHandler, "UA-51549024-1", 1);
-    gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "index.html");
-    gaPlugin.trackEvent( nativePluginResultHandler, nativePluginErrorHandler, "Button", "Click", "event only", 1);
-    gaPlugin.exit(nativePluginResultHandler, nativePluginErrorHandler);
+function onDeviceReady() 
+{
+  // alert("calling ga");
+  gaPlugin = window.plugins.gaPlugin;
+  // alert("calling init");
+  gaPlugin.init(successHandler, errorHandler, "UA-51549024-1", 1);
+  gaPlugin.trackPage(nativePluginResultHandler, nativePluginErrorHandler, "index.html");
+  gaPlugin.exit(nativePluginResultHandler, nativePluginErrorHandler);
 }
 
-function successHandler()
+function successHandler() // for gaPlugin
 {
-    alert("init success");
+  // alert("Init success.");
 }
 
-function errorHandler()
+function errorHandler() // for gaPlugin
 {
-    alert("init failed");
+  // alert("Init failed.");
 }
 
-function nativePluginResultHandler()
+function nativePluginResultHandler() // for gaPlugin
 {
-    alert("tracking success");
+  // alert("Tracking success.");
 }
 
-function nativePluginErrorHandler()
+function nativePluginErrorHandler() // for gaPlugin
 {
-    alert("tracking failed");
+  // alert("Tracking failed.");
 }
